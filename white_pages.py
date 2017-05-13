da898ed08f78d45ffe42ecb81a666152bb02050d
Sarah Jee <sarahjee@Sarahui-MacBook-Pro.local>
2017-05-13 14:37:50 +0900

    Add comments

--- white_pages.py
+++ white_pages.py
@@ -1,15 +1,16 @@
-print ("\n\nWelcome!")
+print ("\n\nWelcome!") # Print single line
 print """
 \nThis program creates text file if you
 type in some informations.
 It won't take too long...
 
 Enjoy!
-"""
-response = raw_input("*** To continue, press ENTER. ***")
+""" # Print multiple lines
 
-if response == "":
-    filename = raw_input("\nFirst, set a Filename:")
+response = raw_input("*** To continue, press ENTER. ***") # Make variable called response with line of texts
+
+if response == "": # If variable eqauls nothing, run the codes below
+    filename = raw_input("\nFirst, set a Filename:") # Make variable called filename with line of texts
     print"""
 
     --------------------------------------------------------
@@ -18,29 +19,29 @@ if response == "":
     --------------------------------------------------------
 
     Now you will type in informations of three people you want...
-    """
-    with open (filename, "w") as f:
+    """ # Print multiple lines
+    with open (filename, "w") as f: # Open file with name that user set, and write
 
-        per1 = raw_input("First person's name:")
-        f.write(per1 + "\n")
-        birth1 = raw_input("First person's birthday:")
-        f.write(birth1 + "\n")
-        pnum1 = raw_input("First person's phonenumber:")
-        f.write(pnum1 + "\n\n\n")
+        per1 = raw_input("First person's name:") # Make variable called per1 with line of texts
+        f.write(per1 + "\n") # Type in per1 with newline inside the text file
+        birth1 = raw_input("First person's birthday:") # Make variable called birth1 with line of texts
+        f.write(birth1 + "\n") # Type in birth1 with newline inside the text file
+        pnum1 = raw_input("First person's phonenumber:") # Make variable called pnum1 with line of texts
+        f.write(pnum1 + "\n\n\n") # Type in pnum1 with multiple newlines inside the text file
 
-        per2 = raw_input("Second person's name:")
-        f.write(per2 + "\n")
-        birth2 = raw_input("Second person's birthday:")
-        f.write(birth2 + "\n")
-        pnum2 = raw_input("Second person's phonenumber:")
-        f.write(pnum2 + "\n\n\n")
+        per2 = raw_input("Second person's name:") # Make variable called per2 with line of texts
+        f.write(per2 + "\n") # Type in per2 with newline inside the text file
+        birth2 = raw_input("Second person's birthday:") # Make variable called birth2 with line of texts
+        f.write(birth2 + "\n") # Type in birth2 with newline inside the text file
+        pnum2 = raw_input("Second person's phonenumber:") # Make variable called pnum2 with line of texts
+        f.write(pnum2 + "\n\n\n") # Type in pnum2 with multiple newlines inside the text file
 
-        per3 = raw_input("Third person's name:" )
-        f.write(per3 + "\n")
-        birth3 = raw_input("Third person's birthday:")
-        f.write(birth3 + "\n")
-        pnum3 = raw_input("Third person's phonenumber:")
-        f.write(pnum3 + "\n\n\n")
+        per3 = raw_input("Third person's name:" ) # Make variable called per3 with line of texts
+        f.write(per3 + "\n") # Type in per3 with newline inside the text file
+        birth3 = raw_input("Third person's birthday:") # Make variable called birth3 with line of texts
+        f.write(birth3 + "\n") # Type in birth3 with newline inside the text file
+        pnum3 = raw_input("Third person's phonenumber:") # Make variable called pnum3 with line of texts
+        f.write(pnum3 + "\n\n\n") # Type in pnum3 with multiple newlines inside the text file
 
-    print ("Thank you. Check the file if it's all saved.")
-    f.close()
+    print ("Thank you. Check the file if it's all saved.") # Print single line
+    f.close() # Save and close the file
